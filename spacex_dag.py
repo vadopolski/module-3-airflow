c8c56f56ea48e8a504c52cb987413f5f7fdf4dac
Vadim <vadopolski@gmail.com>
2021-03-08 10:38:48 +0300

    Test

--- spacex_dag.py
+++ spacex_dag.py
@@ -16,17 +16,19 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
-    dag=dag
-)
+for name_rocket in ["all", "falcon1", "falcon9", "falconheavy"]:
+    t1 = BashOperator(
+        task_id="get_data_" + name_rocket,
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data{}".format(" -r {{ params.rocket }}" if name_rocket !='all' else ""),
+        params={"rocket": name_rocket},
+        dag=dag
+    )
 
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
+    t2 = BashOperator(
+        task_id="print_data_" + name_rocket,
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv",
+        params={"rocket": name_rocket}, # falcon1/falcon9/falconheavy
+        dag=dag
+    )
 
-t1 >> t2
+    t1 >> t2
